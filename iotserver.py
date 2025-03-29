7dcb25200df6d4372e657ba563d261e49a145125
GAYATRI PADHI <138383524+GayatriPadhii@users.noreply.github.com>
2025-03-30 02:15:30 +0530

    Update iotserver.py

--- iotserver.py
+++ iotserver.py
@@ -2,15 +2,19 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return jsonify({"message": "Server is running! Use /store-data to send data."})
+# Store received data
+parking_data = []
 
 @app.route('/store-data', methods=['POST'])
 def store_data():
     data = request.get_json()
-    print("Received data:", data)  # Logs for debugging
+    parking_data.append(data) 
+    print("Received data:", data)
     return jsonify({"status": "success", "received": data}), 200
+
+@app.route('/view-data', methods=['GET'])
+def view_data():
+    return jsonify(parking_data) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
